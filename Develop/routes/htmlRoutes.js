175438af1debece6bc346b83528092b9bf6c3c6c
Bryan Pero <bryanpero@gmail.com>
2020-06-14 15:42:52 -0400

    worked on pseudo code/routes

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,9 @@
+//install packages which may inlcude "path" and "express".  Use "router" from express.
+
+//GET notes route:
+//use the .sendFile() directory, filename
+
+//GET * route:
+//use the .sendFile() directory, filename
+
+//export the router
