d9835e7e85a7b0b2334e6f5990e88841d8a2f8ca
Bryan Pero <bryanpero@gmail.com>
2020-06-14 20:52:14 -0400

    worked on api routes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,9 +1,34 @@
-//install packages which may inlcude "path" and "express".  Use "router" from express.
+//Require express, router, and Notebook class:
+const router = require("express").Router();
+const notebook = require("../db/Notebook");
 
-//GET notes route:
-//use the .sendFile() directory, filename
+//GET route:
 
-//GET * route:
-//use the .sendFile() directory, filename
+router.get("/notes", function (req, res) {
+  notebook
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
 
-//export the router
+//POST route:
+
+router.post("/notes", function (req, res) {
+  notebook
+    .addNotes(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+//DELETE route:
+
+router.delete("/notes/:id", function (req, res) {
+  notebook
+    .deleteNotes(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+//exposrt the router:
+
+module.exports = router;
