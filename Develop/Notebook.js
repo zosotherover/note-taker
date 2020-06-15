7314a25d00aa1041438d894e377cdc5f9eacb9f5
Bryan Pero <bryanpero@gmail.com>
2020-06-14 20:12:31 -0400

    pseudo coding/organizing

---
+++ Develop/Notebook.js
@@ -0,0 +1,17 @@
+//require uuidv⁴ package to generate an id
+// { id : uuidv⁴() ,title , text }
+
+class Notebook {
+  //getNotes()
+  //inside of this fuction READ from the db.json file.  The contents of the json file will be displayed on the page. To read the file = readFile() which is a part of the fs package.
+  //
+  //
+  //
+  //addNoties()
+  //inside of this function you want to write to the file using the writeFile() from the fs module.
+  //
+  //
+  //deleteNotes()
+  //check against all of the notes to see which one has the id you're looking to delete.
+  //call getNotes() then filter the results to find the id you're looking for and return the ones that don't match
+}
---
+++ Develop/routes/apiRouts.js
@@ -0,0 +1,13 @@
+//require express
+//require the class file so you can use the functions that you created
+
+//GET request for /api/notes
+// call the getNotes() from the class that your required
+// res.json(note)
+
+//POST request for /api/notes
+// call the addNotes() from the class that your required
+// res.json(note)
+
+//DELETE request for /api/notes/:id
+//call the deleteNote() function
