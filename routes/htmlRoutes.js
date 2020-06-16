46bd679ace86b103369babda0097b9de41162371
Bryan Pero <bryanpero@gmail.com>
2020-06-16 12:49:52 -0400

    altering folder structure

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,20 @@
 //require express (router), and path:
 
 const router = require("express").Router();
+const express = require("express");
 const path = require("path");
 
 // GET notes route:
 // use the sendFile() directory, filename
 router.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "..", "public", "html", "notes.html");
+  const filePath = path.join(__dirname, "..", "public", "notes.html");
   res.sendFile(filePath);
 });
 
 // GET * route:
 // use the sendFile() directory, filename
 router.get("*", (req, res) => {
-  const filePath = path.join(__dirname, "..", "public", "html", "index.html");
+  const filePath = path.join(__dirname, "..", "public", "index.html");
   res.sendFile(filePath);
 });
 
